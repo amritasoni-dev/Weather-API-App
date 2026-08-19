adbe5fee9a34838f5b3f598302ef62821073d8c0
Amrita Soni <amritasoni10thc@gmail.com>
2026-08-19 11:17:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ async function updateWeatherInfo(city){
     windValueTxt.textContent = speed + `m/s`
     currentDateTxt.textContent = getCurrentDate()
 
-    weatherSummaryImg.src = `assets/assets/weather/${getWeatherIcon(id)}`
+    weatherSummaryImg.src = getWeatherIcon(id)
     
     await updateForecastInfo(city)
     showDisplaySection(weatherInfoSection)
@@ -115,7 +115,7 @@ function updateForecastItems(weatherData){
     const forecastItem = `
         <div class="forecast-item">
                     <h5 class="forecast-item-date regular-txt">${dateResult}</h5>
-                    <img src="assets/assets/weather/${getWeatherIcon(id)}"  class="forecast-item-img">
+                    <img src="getWeatherIcon(id)"  class="forecast-item-img">
                     <h5 class="forecast-item-temp">${Math.round(temp)} °C</h5>
         </div>
     `
